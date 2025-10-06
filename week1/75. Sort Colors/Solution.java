e6fce1d0dc6b8428d231a8662a92c6f02038e485
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-10-05 22:21:32 -0400

    Update Solution.java

--- week1/75. Sort Colors/Solution.java
+++ week1/75. Sort Colors/Solution.java
@@ -1,1 +1,31 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int size = nums.length;
+        int red = 0;
+        int white = 0;
+        int blue = 0;
 
+        for (int i = 0; i < size; i++) {
+            if (nums[i] == 0){
+                red++;
+            }
+            else if (nums[i] == 1){
+                white++;
+            }
+            else{
+                blue++;
+            }
+        }
+
+        int index = 0;
+        for (int i=0; i<red; i++){
+            nums[index++]=0;
+        }   
+        for (int i=0; i<white; i++){
+            nums[index++]=1;
+        }   
+        for (int i=0; i<blue; i++){
+            nums[index++]=2;
+        }
+    }
+}
