b807de8f2eb408a707675f5f4f76c67048a4ae9d
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-10-13 19:35:51 -0400

    Update Solution.java

--- week2/8. String to Integer/Solution.java
+++ week2/8. String to Integer/Solution.java
@@ -1,1 +1,31 @@
+class Solution {
+    public int myAtoi(String s) {
+        long num = 0;
+        int i = 0, sign = 1;
 
+        while (i<s.length() && s.charAt(i) == ' ')
+        {
+            i++;
+        }
+
+        if (i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+'))
+        {
+            if (s.charAt(i) == '-') sign = -1;
+            i++;
+        }
+
+        for (; i < s.length() && Character.isDigit(s.charAt(i)); i++)
+        {
+            num = num * 10 + (s.charAt(i) - '0');
+            if (sign * num >= Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            if (sign * num <= Integer.MIN_VALUE)
+            {
+                return Integer.MIN_VALUE;
+            }
+        }
+
+        return (int)(sign * num);
+    }
+}
