7e1f630f629d97ea963fd05208d047b0c1d23550
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-10-13 20:20:26 -0400

    Update Solution.java

--- week2/438. Find All Anagrams in a String/Solution.java
+++ week2/438. Find All Anagrams in a String/Solution.java
@@ -1,1 +1,43 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() < p.length()){
+            return result;
+        }
 
+        int[] count = new int[26];
+        for (char c : p.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        int left = 0, right = 0, len = p.length();
+        int remaining = len;
+
+        while (right < s.length()) {
+            char rChar = s.charAt(right);
+            if (count[rChar - 'a'] > 0)
+            {
+                remaining--;
+            }
+            count[rChar - 'a']--;
+            right++;
+
+            if (remaining == 0)
+            {
+                result.add(left);
+            }
+
+            if (right - left == len) {
+                char lChar = s.charAt(left);
+                if (count[lChar - 'a'] >= 0)
+                {
+                    remaining++;
+                }
+                count[lChar - 'a']++;
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
