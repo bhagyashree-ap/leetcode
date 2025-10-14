ac641ca9e22eb1039cd1e73a462d226a07cc7502
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-10-13 22:21:59 -0400

    Update Solution.java

--- week2/151. Reverse Words in a String/Solution.java
+++ week2/151. Reverse Words in a String/Solution.java
@@ -1,1 +1,23 @@
+class Solution {
+    public String reverseWords(String s) {
+        List<String> words = new ArrayList<>();
+        int len=s.length();
+        int right =0;
+        
+        for(int i=0; i<len; ){
+            while (i<len && s.charAt(i) == ' ') i++;
+            if (i >= len) break;
+           
+            right=i;
 
+            while(right<len && s.charAt(right) != ' ')right++;
+
+            String subword = s.substring(i, right);
+            words.add(subword);
+            
+            i=right+1;
+        }      
+        Collections.reverse(words);
+        return String.join(" ", words); 
+    }
+}
