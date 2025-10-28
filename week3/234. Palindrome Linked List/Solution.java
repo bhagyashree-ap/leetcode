50a14d9ad52802854d57ce35c76459acd988cd8b
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-10-28 17:20:20 -0400

    Update Solution.java

--- week3/234. Palindrome Linked List/Solution.java
+++ week3/234. Palindrome Linked List/Solution.java
@@ -1,1 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
 
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null, curr = slow.next;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        ListNode first = head, second = prev;
+        boolean palindrome = true;
+        while (second != null) {
+            if (first.val != second.val) {
+                palindrome = false;
+                break;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        curr = prev;
+        prev = null;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        slow.next = prev;
+
+        return palindrome;
+    }
+}
