114ae9e579851bc8964d73033e5a1148173ac367
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-11-04 16:03:28 -0500

    Update Solution.java No. of People Aware of Secret

--- week4/2327. Number of People Aware of a Secret/Solution.java
+++ week4/2327. Number of People Aware of a Secret/Solution.java
@@ -1,1 +1,23 @@
+class Solution {
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        final int MOD = 1_000_000_007;
+        long[] dp = new long[n + 2];
+        long[] prefix = new long[n + 2];
+        dp[1] = 1;
+        prefix[1] = 1;
 
+        for (int day = 2; day <= n; day++) {
+            int start = Math.max(0, day - forget);
+            int share = Math.max(0, day - delay);
+            dp[day] = (prefix[share] - prefix[start] + MOD) % MOD;
+            prefix[day] = (prefix[day - 1] + dp[day]) % MOD;
+        }
+
+        long result = 0;
+        for (int i = n - forget + 1; i <= n; i++) {
+            if (i > 0) result = (result + dp[i]) % MOD;
+        }
+
+        return (int) result;
+    }
+}
