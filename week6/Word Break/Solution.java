16128d08a753cbf927912aa588f1d866248d1675
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-11-11 18:34:02 -0500

    Update Solution.java word break

--- week6/Word Break/Solution.java
+++ week6/Word Break/Solution.java
@@ -1,1 +1,24 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        Set<Integer> visited = new HashSet<>();
 
+        heap.add(0);
+
+        while (!heap.isEmpty()) {
+            int start = heap.poll();
+            if (visited.contains(start)) continue;
+            visited.add(start);
+
+            for (int end = start + 1; end <= s.length(); end++) {
+                if (wordSet.contains(s.substring(start, end))) {
+                    if (end == s.length()) return true;
+                    heap.add(end);
+                }
+            }
+        }
+
+        return false;
+    }
+}
