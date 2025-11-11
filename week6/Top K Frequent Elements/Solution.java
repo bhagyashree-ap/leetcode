b40c2ed28dbfbbccc3e87d1f3ebc9e76853351e9
Bhagyashree Patil <84381569+bhagyashree-ap@users.noreply.github.com>
2025-11-11 18:30:09 -0500

    Update Solution.java top k frequent elements

--- week6/Top K Frequent Elements/Solution.java
+++ week6/Top K Frequent Elements/Solution.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for(int num:nums){
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0)+1);
+        }
 
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>((a, b) -> frequencyMap.get(b) - frequencyMap.get(a));
+
+        maxHeap.addAll(frequencyMap.keySet());
+
+        int[] result = new int[k];
+        for(int i=0;i<k;i++){
+            result[i]=maxHeap.poll();
+        }
+
+     return result;   
+    }
+}
